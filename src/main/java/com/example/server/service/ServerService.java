ca88874ae290058d8a5f21ad337d30bcc925b251
Huy Hoang <hhoang2197@gmail.com>
2022-04-24 11:37:13 -0500

    Inital project commit

---
+++ src/main/java/com/example/server/service/ServerService.java
@@ -0,0 +1,15 @@
+package com.example.server.service;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import com.example.server.model.Server;
+
+public interface ServerService {
+    Server create(Server server);
+    Server ping(String ipAddress) throws IOException;
+    Collection<Server> list(int limit);
+    Server get(long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+}
